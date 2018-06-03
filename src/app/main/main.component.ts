c36e513cd446f9417080997b6c768c943b26df3d
tatianap1991 <tatiana.pavlov1991@gmail.com>
2018-06-03 10:40:37 +0300

    Basic grid with 12 columns was added using TypeScript

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
   tiles = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+    {text: '', cols: 1, color: 'transparent'},
+    {text: 'One', cols: 10,  color: 'lightblue'},
+    {text: '', cols: 1,  color: 'transparent'}
+
+
+
   ];
 
   constructor() { }
